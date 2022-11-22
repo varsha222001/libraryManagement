c80aed0a447952e42f7a5d165169497cfe9ba670
RakeshTirumala <rakeshhtirumala@gmail.com>
2022-11-22 14:41:29 +0530

    making it better!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Spinner from "react-spinkit";
 
 function App() {
   return (
+    <>
     <nav className='title'>Library Management</nav>
+    <center style={{padding:"10%"}}>
+      <Spinner name="cube-grid" style={{ width: 100, height: 100,  color:"whitesmoke"}}/>
+    </center>
+    </>
   );
 }
 
